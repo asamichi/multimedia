778fe0d8db19c82639099688a8b115190b9ff40a
asamichi <crow.foxcharmer@gmail.com>
2017-01-31 02:34:58 +0900

    1/31

--- html/ue.js
+++ html/ue.js
@@ -1,3 +1,3 @@
-document.write('<FONT size="2" color="#30cb1f" >アニメ・ゲーム・pcとかのサイト</FONT><br>'+
-'<FONT size="3" color="#30cb1f" face="ＭＳ 明朝"><b>黄昏月～Twilight moon～　</b><br></FONT>'+
-'<hr><br>');
+document.write('<FONT size="2" color="#30cb1f" >アニメ・ゲーム・pcとかのサイト</FONT><br>');
+document.write('<FONT size="3" color="#30cb1f" face="ＭＳ 明朝"><b>黄昏月～Twilight moon～　</b><br></FONT>');
+document.write('<hr><br>');
